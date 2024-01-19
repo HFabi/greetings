c3e5583c38bd47faf967abc44d3378309e91779a
Fabian <fabianheck@MacBook-Pro-von-Fabian.local>
2024-01-19 21:17:32 +0100

    feat: add submodule

--- src/main.py
+++ src/main.py
@@ -1,5 +1,12 @@
 
-from greetings.plugins.text import say_hello
+import sys
+import os
+
+relative_lib_path = os.path.join(os.path.dirname(__file__), '../plugins/greetings-de/src')
+lib_path = os.path.abspath(relative_lib_path)
+sys.path.append(lib_path)
+
+from text import say_hello
 
 
 def main():
